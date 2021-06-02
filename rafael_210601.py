e1f294839c48c34efe8e3686d00f5b7d2c08f299
rafaelzanirato <84473402+rafaelzanirato@users.noreply.github.com>
2021-06-01 22:03:00 -0300

    Add files via upload

---
+++ rafael_210601.py
@@ -0,0 +1,46 @@
+# Question 2
+lista = ["toyota","audi","bmw","ferrari","ford","tesla"]
+# Question 3
+print(lista[2])
+# Question 4
+lista.append("bicycle")
+# Question 5
+print(lista)
+# Question 6
+lista[0]= "rafael"
+# Question 7
+print(lista)
+# Question 8
+xis= lista[0] + " " +lista[1]
+# Question 9
+print(xis)
+# Question 10
+last= lista[6].upper()
+# Question 11
+print(last)
+# Question 12
+lista.insert(3,"nova odessa")
+# Question 13 
+print(lista)
+# Question 14
+del lista[2]
+# Question 15
+print (lista)
+# Question 16
+popado= lista.pop(6)
+# Question 17
+print(lista)
+print(popado)
+# Question 18
+zero= lista.pop(0)
+# Question 19
+print(lista)
+print(zero)
+# Question 20
+fruits= ["melon","banana","apple"]
+# Question 21
+print(fruits)
+# Question 22
+fruits.sort()
+# Question 23
+print(fruits)
